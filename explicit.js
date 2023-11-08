52270692608d21bd834a71e9cfa661a26cef0e2d
saijyothi-123 <115785642+saijyothi-123@users.noreply.github.com>
2023-11-08 09:29:05 -0500

    Add files via upload

---
+++ explicit.js
@@ -0,0 +1,19 @@
+//Explicity 
+
+const type = Number("15");
+console.log(typeof type)
+
+
+
+const type1 = String(155);
+console.log(typeof type1)
+
+
+
+const type2=Boolean(12);
+console.log(typeof type2)
+
+
+
+const type3=Number(true);
+console.log(type3)
---
+++ implicit.js
@@ -0,0 +1,23 @@
+//implicity 
+
+let n = 15;
+let s = "Tanu";
+console.log(n+s)
+
+
+let n1=15;
+let n2="15";
+console.log(n2+n1)
+
+
+
+const n3 = 36;
+const n4 = "10";
+console.log(n3-n4)
+console.log(n3*n4)
+console.log(n3/n4)
+
+let num1="30"
+let num2="10"
+let result=num1+num2;
+console.log(result);

33204edae89d402157b8f0a9c21ffd1f161cd34c
saijyothi-123 <115785642+saijyothi-123@users.noreply.github.com>
2023-11-08 09:28:15 -0500

    Add files via upload

---
+++ variable9.js
@@ -0,0 +1,19 @@
+// create two veriable
+
+const nm1=10;
+const nm2="5";
+const nm1AsString=String(nm1);
+const nm2AsNumber = Number(nm2);
+
+//sum of two numbers
+
+
+const sum = nm1AsString+nm2AsNumber;
+console.log(sum)
+
+
+//product of two numbers
+
+
+const product = nm1*nm2;
+console.log(product)

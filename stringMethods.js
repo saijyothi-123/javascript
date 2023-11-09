e4b237d2f8cf963c9e15cdfb64f37c6638e0c5e4
saijyothi-123 <115785642+saijyothi-123@users.noreply.github.com>
2023-11-08 23:57:32 -0500

    Add files via upload

---
+++ stringMethods.js
@@ -0,0 +1,21 @@
+
+let text="Vijay Thalapathy"
+
+let result = text.length;
+console.log(result);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.endsWith('thy'));
+console.log(text.startsWith('vijay'));
+console.log(text.charAt(1));
+console.log(text.charAt(text.length-3));
+console.log(text.indexOf("5"));
+console.log(text.indexOf("V"));
+console.log(text.indexOf("z"));
+console.log(text.trim());
+console.log(text.trim().toLowerCase().startsWith("vijay"));
+console.log(text.includes("Thala"));
+console.log(text.includes("thala"));
+console.log(text.slice(0,9));
+console.log(text.slice(-3));
+console.log(text.slice(-5));
